32cc1e2868d5fb6aef27e4d5f803e847f1eda977
Josef Hedblom <hedblomjosef@gmail.com>
2021-05-15 23:21:02 +0200

    Add dotenv.

--- api/app.js
+++ api/app.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const express   = require('express');
 const app       = express();
 const cors      = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server runing on port: ${PORT}`));
