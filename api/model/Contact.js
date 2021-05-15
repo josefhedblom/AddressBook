105e6e9c304a58fc959f92d2eb32831319a8f740
Josef Hedblom <hedblomjosef@gmail.com>
2021-05-15 23:34:50 +0200

    Add Contact Schema

---
+++ api/model/Contact.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = mongoose.Schema({
+    first_name: {
+        type: String
+    },
+    last_name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    country: {
+        type: String
+    }
+});
+
+const Contact = mongoose.model('Contact', ContactSchema);
+module.exports = Contact;
