07b9f609ddf7eaa9f730acfbf78e31ebcb237d94
Josef Hedblom <hedblomjosef@gmail.com>
2021-05-16 20:56:07 +0200

    Add contoller and router to get one user.

--- api/routes/contacts.js
+++ api/routes/contacts.js
@@ -2,6 +2,7 @@ const router            = require('express').Router();
 const ContactController = require('../controllers/contacts');
 
 router.get('/', ContactController.show);
+router.get('/:first_name', ContactController.contact);
 router.post('/add', ContactController.add);
 
 
